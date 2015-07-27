450e6ba642720b4d4e64dd911be778ac273440d2
Kenneth Berland <ken@grandrounds.com>
2015-07-27 16:56:46 -0700

    add 138a:0017 Validity Sensors, Inc.

--- libfprint/drivers/vfs5011.c
+++ libfprint/drivers/vfs5011.c
@@ -869,7 +869,9 @@ static void dev_deactivate(struct fp_img_dev *dev)
 
 static const struct usb_id id_table[] =
 {
+  //  Bus 001 Device 004: ID 138a:0017 Validity Sensors, Inc. 
 	{ .vendor = 0x138a, .product = 0x0011 /* vfs5011 */ },
+	{ .vendor = 0x138a, .product = 0x0017 /* vfs5011 */ },
 	{ .vendor = 0x138a, .product = 0x0018 /* vfs5011 */ },
 	{ 0, 0, 0, },
 };
